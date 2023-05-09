039c55e5e94249e92b4b7bbd1aba90de4a5fbec7
pedroquintao <pedro.h.quintao@hotmail.com>
2023-05-09 18:49:34 -0300

    Aula 02 finalizada
    -Foi implementada a função aplicarDesconto, que aplica um desconto a cada livro da lista

--- app/main.js
+++ app/main.js
@@ -6,13 +6,16 @@ getBuscarLivrosDaApi();
 
 async function getBuscarLivrosDaApi() {
     const res = await fetch(endPoinDaApi);
-    livros =  await res.json(); //Entender melhor porque é necessário utilizar o await nessa linha
-    adicionaLivros(livros);
+    livros =  await res.json(); 
+
+    let livrosComDesconto = aplicarDesconto(livros);
+
+    exibeLivrosNaTela(livrosComDesconto);
 
     console.table(livros);
 }
 
-function adicionaLivros(secao) {
+function exibeLivrosNaTela(secao) {
 
     secao.forEach(livro => {
         
@@ -22,11 +25,20 @@ function adicionaLivros(secao) {
           ${livro.titulo}:
         </h2>
         <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
+        <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
         <div class="tags">
           <span class="tag">${livro.tag}</span>
         </div>
-      </div>`
-        secaoDeLivros.innerHTML += livroAtual
+      </div>`//A função numeroEntrada.toFixed(n) retorna o numeroEntrada limitado com n casas depois da vírgula
+        secaoDeLivros.innerHTML += livroAtual;
     })
+}
+
+function aplicarDesconto(livros) {
+  const desconto = 0.2
+
+  livrosDes = livros.map(l => {
+    return {...l, preco: l.preco - l.preco * desconto}});//Spread operator ... faz uma cópia do objeto existente para outro objeto e altera a propriedade que está depois da vírgula. A ação a ser feita é colocada depois dos :
+
+  return livrosDes;
 }
