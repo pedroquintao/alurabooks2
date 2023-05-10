23d501bf23a90be6e9978554a7cd23ac3bc4f608
pedroquintao <pedro.h.quintao@hotmail.com>
2023-05-10 15:37:40 -0300

    Minha versão pessoal do filtro
    -Essa versão do filtro está funcional e foi feita antes de assistir as aulas após a aula 'Aula 03 -filter'. Em sequência farei uma versão mais próxima da passada em aula

--- app/main.js
+++ app/main.js
@@ -1,16 +1,12 @@
-let livros = [{imagem: 'imagens/O-Retorno-do-cangaceiro-JavaScript.png',
-              alt: 'capa do do livro abacatero',
-              titulo: 'Abacatero',
-              autor: 'Valcir Carrasco',
-              preco: 66.66,
-              tag: 'front-end'}];
-
+let livros = [];
 const endPoinDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 const secaoDeLivros = document.getElementById('livros');
 const filtros = document.querySelectorAll('.btn');
-console.log('%cmain.js line:11 filtros', 'color: #007acc;', filtros);
-// getBuscarLivrosDaApi();
+
+getBuscarLivrosDaApi();
+
 adicionaListernerNosFiltros(filtros);
+
 async function getBuscarLivrosDaApi() {
     const res = await fetch(endPoinDaApi);
     livros =  await res.json(); 
@@ -22,7 +18,18 @@ async function getBuscarLivrosDaApi() {
     console.table(livros);
 }
 
+function aplicarDesconto(livros) {
+  const desconto = 0.2
+
+  livrosDes = livros.map(l => {
+    return {...l, preco: l.preco - l.preco * desconto}});//Spread operator ... faz uma cópia do objeto existente para outro objeto e altera a propriedade que está depois da vírgula. A ação a ser feita é colocada depois dos :
+
+  return livrosDes;
+}
+
 function exibeLivrosNaTela(secao) {
+
+    secaoDeLivros.innerHTML = '';
 
     secao.forEach(livro => {
         
@@ -34,32 +41,41 @@ function exibeLivrosNaTela(secao) {
         <p class="livro__descricao">${livro.autor}</p>
         <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
         <div class="tags">
-          <span class="tag">${livro.tag}</span>
+          <span class="tag">${livro.categoria}</span>
         </div>
       </div>`//A função numeroEntrada.toFixed(n) retorna o numeroEntrada limitado com n casas depois da vírgula
         secaoDeLivros.innerHTML += livroAtual;
     })
 }
 
-function aplicarDesconto(livros) {
-  const desconto = 0.2
-
-  livrosDes = livros.map(l => {
-    return {...l, preco: l.preco - l.preco * desconto}});//Spread operator ... faz uma cópia do objeto existente para outro objeto e altera a propriedade que está depois da vírgula. A ação a ser feita é colocada depois dos :
-
-  return livrosDes;
-}
 
 function adicionaListernerNosFiltros(filtros) {
-  filtros.forEach(element => element.addEventListener('click', function() {
-    console.log('%cmain.js line:55 this', 'color: #007acc;', this);
-    console.log('%cmain.js line:55 this.value', 'color: #007acc;', this.value);
-    const listaDeLivrosFiltrados = filtraLivros(livros, this.value);
-    exibeLivrosNaTela(listaDeLivrosFiltrados) //tentar colocar o toLowerCase aqui
-  }))
+
+  filtros.forEach((element => element.addEventListener('click', function() {
+
+    element.value? 
+    exibeLivrosNaTela(filtraLivros(livros, this.value)) : 
+    getBuscarLivrosDaApi(); //Essa linha executa a mesma ação do if comentado abaixo
+
+
+    // if(element.value){
+      
+    //   const listaDeLivrosFiltrados = filtraLivros(livros, this.value);
+      
+    //   exibeLivrosNaTela(listaDeLivrosFiltrados);
+
+    // }
+
+    // else {
+
+    //   getBuscarLivrosDaApi();
+
+    // }
+
+  })))
 }
 
 function filtraLivros(lista, tipo) {
-  const tipoMinusculo = tipo.toLowerCase();//talves não seja necessário
-  return lista.filter(livro => {return livro.tag === tipo})
+  
+  return lista.filter(livro => {return livro.categoria === tipo});
 }
